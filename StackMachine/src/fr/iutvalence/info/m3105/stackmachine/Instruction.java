69adc8e12b0b3f2958daffaf2f6730a1208370da
NumaCrozier <draconk_26@hotmail.fr>
2015-10-01 17:30:46 +0200

    Added : classes and methods that were missing in MachineLauncher.java and Machine.java

--- StackMachine/src/fr/iutvalence/info/m3105/stackmachine/Instruction.java
+++ StackMachine/src/fr/iutvalence/info/m3105/stackmachine/Instruction.java
@@ -2,7 +2,23 @@ package fr.iutvalence.info.m3105.stackmachine;
 
 public class Instruction {
 
-	private CPU cpu;
+	private int operator;
+	private int[] addresses;
+	
+	public Instruction(int operator_p, int[] adddresses_p){
+		this.operator = operator_p;
+		this.addresses = adddresses_p;
+	}
+
+	public Object getOpCode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int[] getParams() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	
 }
--- StackMachine/src/fr/iutvalence/info/m3105/stackmachine/Stack.java
+++ StackMachine/src/fr/iutvalence/info/m3105/stackmachine/Stack.java
@@ -1,6 +1,12 @@
 package fr.iutvalence.info.m3105.stackmachine;
 
 public class Stack {
+	
+	private int stackSize;
+
+	public Stack(int stackSize_p) {
+		this.stackSize = stackSize_p;
+	}
 
 	private int[] content;
 	
